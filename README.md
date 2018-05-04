5dba92b4f0631c43de6d3074de1dfda7c60feb3d
Komron <aripovkamron@gmail.com>
2018-05-04 23:12:56 +0200

    new '/list' command

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # gpa-calculator
 Calculate your GPA on a 4.0 scale based on your grades.
 
+## Prerequisites
+Install all the dependencies with:
+
+`pip install -r requirements.txt`
+
+Also, you will need to create a `config.py` file with `API` variable
+
 ## How to run
 Linux: `python3 main.py`
+
 Windows: `python main.py`
+
+## Available commands
+`/start` - Greeting message with input example
+
+`/list` - See all the current entries
+
+`/done` - Send calculated GPA and empty entered list of subjects and grades
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 import config
+import values
 import dbhandler
 from textparser import check_entry, parse_entry
 
@@ -10,19 +11,19 @@ def calculate_gpa(subject_list):
 
     total_gpa = 0 # a counter
     for grade in grade_list:
-        total_gpa += config.GRADES[grade]
+        total_gpa += values.GRADES[grade]
 
     return total_gpa / len(grade_list) # average GPA
 
 
 @bot.message_handler(commands=['start']) # IDEA: FORCE REPLY??
 def send_start(message):
-    bot.send_message(message.chat.id, config.ENTER_REPLY, parse_mode='HTML')
+    bot.send_message(message.chat.id, values.ENTER_REPLY, parse_mode='HTML')
 
 @bot.message_handler(func=lambda m: '-' in m.text, content_types=['text'])
 def record_results(message):
     if not check_entry(message.text):
-        bot.send_message(message.chat.id, config.FORMAT_REPLY)
+        bot.send_message(message.chat.id, values.FORMAT_REPLY)
         return # no need to add the message to db as it is incorrect
 
     subject_list = parse_entry(message.text)
@@ -30,19 +31,36 @@ def record_results(message):
     dbhandler.create_table(message)
     dbhandler.write_table(subject, grade, message)
 
+@bot.message_handler(commands=['list'])
+def send_list(message):
+    data = dbhandler.pull_data(message)
+
+    if not data: # warning is sent if list is empty
+        bot.send_message(message.chat.id, values.EMPTY_REPLY)
+        return
+
+    # data = [['History', 'A'], ['Math', 'A'], ['English', 'A+']]
+    # pair = ['History', 'A']
+
+    string = "Here are your current entries:\n"
+    for pair in data:
+        string += ' - '.join(pair)
+        string += '\n'
+
+    bot.send_message(message.chat.id, string)
+
+
 
 @bot.message_handler(commands=['done'])
 def send_results(message):
     data = dbhandler.pull_data(message)
 
     if not data:
-        bot.send_message(message.chat.id, config.EMPTY_REPLY)
+        bot.send_message(message.chat.id, values.EMPTY_REPLY)
         return
-        
+
     average_GPA = str(round(calculate_gpa(data), 2)) # GPA is 2 d.p.
-
-    bot.send_message(message.chat.id, config.GPA_REPLY.format(average_GPA))
-
+    bot.send_message(message.chat.id, values.GPA_REPLY.format(average_GPA))
     dbhandler.delete_table(message) # so user can enter new classes next time
 
 bot.polling()
